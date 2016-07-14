e37c1ac5207600622bf0560cb6c6723551a5d32f
makeinjava <makeinjava@gmail.com>
2016-07-14 09:25:23 +0530

    Updated the data of tree node

--- ReverseTreePrint(bfs)/src/main/java/org/learn/Client/App.java
+++ ReverseTreePrint(bfs)/src/main/java/org/learn/Client/App.java
@@ -6,15 +6,15 @@ import org.learn.Question.ReverseLevelOrder;
 public class App {
 	public static void main(String[] args) {
 		// root level 0
-		Node A = Node.createNode(100);
+		Node A = Node.createNode(60);
 		// Level 1
 		Node B = Node.createNode(50);
-		Node C = Node.createNode(150);
+		Node C = Node.createNode(90);
 		// Level 2
 		Node D = Node.createNode(25);
 		Node E = Node.createNode(80);
-		Node F = Node.createNode(125);
-		Node G = Node.createNode(160);
+		Node F = Node.createNode(75);
+		Node G = Node.createNode(45);
 
 		// connect Level 0 and 1
 		A.left = B;
@@ -24,7 +24,7 @@ public class App {
 		B.right = E;
 		C.left = F;
 		C.right = G;
-		System.out.println("Reverse Level Order Traversal");
+		System.out.println("Reverse Level Order Traversal:");
 		ReverseLevelOrder.reverseLevelOrder(A);
 	}
 }
